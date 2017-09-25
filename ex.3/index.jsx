d86feb5d903ff9446a8bca622882e3f78103df00
Fabio Galante Mans <fabio.mans@superdigital.com.br>
2017-09-25 15:38:25 -0300

    Novos ex

--- ex.3/index.jsx
+++ ex.3/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Component from './component.jsx'
+import Component from './component'
 
 ReactDOM.render(<Component />, document.getElementById('app'))
